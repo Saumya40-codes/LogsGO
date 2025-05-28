5778ed3533e23054d54a2652610f22300d030b3e
Saumya Shah <saumyabshah90@gmail.com>
2025-05-28 19:25:47 +0530

    add mantine ui
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- pkg/ui/src/App.tsx
+++ pkg/ui/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
-    <>
-      LogsGo
-    </>
+    <MantineProvider>
+      <div className="App">
+        <h1>LogsGo</h1>
+        <p>A standalone log ingestion and querying tool.</p>
+      </div>
+    </MantineProvider>
   )
 }
 
