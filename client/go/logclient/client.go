f374da409aed0efdfc2aa94a066a286484e891fe
Saumya Shah <saumyabshah90@gmail.com>
2025-07-08 19:38:22 +0530

    update client lib for inbuilt support to upload to queue
    Signed-off-by: Saumya Shah <saumyabshah90@gmail.com>

--- client/go/logclient/client.go
+++ client/go/logclient/client.go
@@ -4,17 +4,29 @@ import (
 	"context"
 	"crypto/tls"
 	"errors"
+	"fmt"
 	"time"
 
 	logapi "github.com/Saumya40-codes/LogsGO/api/grpc/pb"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/proto"
 )
 
 type Client struct {
-	conn   *grpc.ClientConn
-	client logapi.LogIngestorClient
+	conn     *grpc.ClientConn
+	amqpConn *amqp.Connection
+	amqpChan *amqp.Channel
+	client   logapi.LogIngestorClient
+	qOpts    *QueueOpts
+}
+
+// Options for message queue
+type QueueOpts struct {
+	QueueName string
+	Url       string
 }
 
 // NewClient creates a new gRPC client to the log server.
@@ -29,6 +41,53 @@ func NewLogClient(ctx context.Context, addr string) (*Client, error) {
 		conn:   conn,
 		client: logapi.NewLogIngestorClient(conn),
 	}, nil
+}
+
+func NewLogClientWithQueue(ctx context.Context, addr string, qCfg *QueueOpts, insecure bool, tlsCfg *tls.Config) (*Client, error) {
+	var grpcClient *Client
+	var err error
+	if insecure {
+		grpcClient, err = NewLogClient(ctx, addr)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		grpcClient, err = NewLogClientWithTLS(ctx, addr, tlsCfg)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	grpcClient.qOpts = qCfg
+	conn, err := amqp.Dial(qCfg.Url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to RabbitMQ: %w", err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("failed to open channel: %w", err)
+	}
+
+	_, err = ch.QueueDeclare(
+		qCfg.QueueName,
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		conn.Close()
+		ch.Close()
+		return nil, fmt.Errorf("failed to declare queue: %w", err)
+	}
+
+	grpcClient.amqpConn = conn
+	grpcClient.amqpChan = ch
+
+	return grpcClient, nil
 }
 
 // NewClientWithTLS creates a new gRPC client to the log server with TLS credentials.
@@ -46,9 +105,26 @@ func NewLogClientWithTLS(ctx context.Context, addr string, tlsCfg *tls.Config) (
 	}, nil
 }
 
-// Close closes the underlying gRPC connection.
+// Close closes the underlying gRPC connection and connection to message queue (if used)
 func (c *Client) Close() error {
-	return c.conn.Close()
+	err := c.conn.Close()
+	if err != nil {
+		return err
+	}
+
+	if c.amqpConn != nil {
+		err = c.amqpChan.Close()
+		if err != nil {
+			return err
+		}
+
+		err = c.amqpConn.Close()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // UploadLog sends a log entry to the server.
@@ -96,6 +172,35 @@ func (c *Client) UploadLogs(ctx context.Context, entries *logapi.LogBatch) error
 		return errors.New("failed to upload logs")
 	}
 
+	return nil
+}
+
+func (c *Client) UploadLogsToQueue(ctx context.Context, entries *logapi.LogBatch) error {
+	if err := ValidateLogs(entries); err != nil {
+		return err
+	}
+
+	marshalLogs, err := proto.Marshal(entries)
+	if err != nil {
+		return err
+	}
+
+	err = c.amqpChan.PublishWithContext(
+		ctx,
+		"",
+		c.qOpts.QueueName,
+		false,
+		false,
+		amqp.Publishing{
+			DeliveryMode: amqp.Persistent,
+			ContentType:  "application/octet-stream",
+			Body:         []byte(marshalLogs),
+		},
+	)
+
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
